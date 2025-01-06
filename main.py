100c3f0938cc1211094d1f1f9ab888d39c667b31
pizornpy <jaun@LAPTOP-36N6JE8J>
2025-01-06 14:09:29 +0100

    added reading

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    with open(r"/mnt/c/Users/Juan/workspace/github.com/pizornpy/bookbot/books/frankenstein.txt") as f: 
+        file_contents = f.read()
+    
+    print(file_contents)
+
+
+main()
