ed4f971802e84328eb958b436605c32ab265e6f2
Juan <juanjulionn@gmail.com>
2025-01-06 14:12:45 +0100

    added count words

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def main():
     with open(r"/mnt/c/Users/Juan/workspace/github.com/pizornpy/bookbot/books/frankenstein.txt") as f: 
         file_contents = f.read()
     
+    amt_words = count_words(file_contents)
     print(file_contents)
+    print(amt_words)
 
+def count_words(string):
+
+    return len(string.split())
 
 main()
